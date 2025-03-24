f44180100e06eaabe8a94b9ffdaaf5d678ba6444
Axel Hernandez <axel.hernandez@correo.ucu.edu.uy>
2025-03-24 18:28:36 -0300

    Ordenamiento de archivos y TA3 Cont.PAlabras

---
+++ UT1/TA3/ContadorPalabras.java
@@ -0,0 +1,41 @@
+package ContadorPalabras;
+import java.util.Scanner;
+
+//Metodo de la clase ContadorPalabras cuenta la cantidad de palabras en una frase.
+class ContadorPalabras {
+    public int contarPalabras(String frase) {
+        if (frase == null) {
+            return 0;
+        }
+
+        frase = frase.trim();
+        if (frase.isEmpty()) {
+            return 0;
+        }
+        
+        int contador = 0;
+
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+
+            //Chequea si es una letra o un espacio.
+            if (Character.isWhitespace(c)) {
+                contador++;
+            }
+        }
+
+        return (contador +1); // Devuelvo la cantidad de espacios +1 que es la cantidad de palabras, ya que la ultima palabra no tiene espacio despues.
+    }
+}
+
+class Principal {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        ContadorPalabras contador = new ContadorPalabras(); 
+        System.out.println("Ingrese una frase: ");
+        String frase =  scanner.nextLine();
+        int cantidad = contador.contarPalabras(frase); 
+        System.out.println("La cantidad de palabras es: " + cantidad); 
+        scanner.close();
+    }
+}

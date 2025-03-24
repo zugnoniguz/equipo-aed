772b7328314ac3eec65aee90821a617169b26aa5
iaravitabar <iara.vitabar@correo.ucu.edu.uy>
2025-03-24 19:11:16 -0300

    ejsSubEquipB

--- UT1/TA3/ContadorPalabras.java
+++ UT1/TA3/ContadorPalabras.java
@@ -29,7 +29,39 @@ class ContadorPalabras {
 
         return (contador +1); // Devuelvo la cantidad de espacios +1 que es la cantidad de palabras, ya que la ultima palabra no tiene espacio despues.
     }
+
+//Subequipo “B”: Obtener la cantidad de palabras con largo mayor a X caracteres
+// Para esta funcionalidad al metodo ContadorPalabras se le agrega un parametro entero que indica la cantidad de caracteres minimos que debe tener una palabra para ser contada.
+// Se agrega un metodo que cuenta la cantidad de palabras con largo mayor a X caracteres.
+
+    public int contarCaracteresX(String frase, int num){
+        if (frase == null) {
+            return 0;
+        }
+
+        frase = frase.trim();
+        if (frase.isEmpty()) {
+            return 0;
+        }
+        int contadorLetras = 0;
+        int contadorPalabras = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+
+            if (Character.isWhitespace(c)) {
+                if(contadorLetras > num){
+                    contadorPalabras++;
+                }
+                contadorLetras = 0;
+            }else{
+                contadorLetras++;
+            }
+        }
+        return contadorPalabras;
+    }
 }
+
+
 
 class Principal {
     public static void main(String args[]) {
@@ -39,6 +71,10 @@ class Principal {
         String frase =  scanner.nextLine();
         int cantidad = contador.contarPalabras(frase); 
         System.out.println("La cantidad de palabras es: " + cantidad); 
+        System.out.println("Ingrese la cantidad de caracteres minimos que debe tener una palabra para ser contada: ");
+        int num = scanner.nextInt();
+        int cantidadCaracteres = contador.contarCaracteresX(frase, num);
+        System.out.println("La cantidad de palabras con largo mayor a " + num + " caracteres es: " + cantidadCaracteres);
         scanner.close();
     }
 }
